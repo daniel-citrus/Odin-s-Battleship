24a148c0245554cd709655042166df38018f0104
Daniel <dandcalvo@gmail.com>
2024-12-19 10:19:40 -0800

    pseudocode for game loop

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
 import '../style/style.css';
+
+// game loop
+// set current player: 1
+// attack player 2
+// check if all ships on player 2 have been sunk
+// if all sunk, player 1 wins
+// else switch current player
+// if mode is 'computer,' auto attack for computer
+
+
+
+function startGame() { }
+function playerWins() { }
